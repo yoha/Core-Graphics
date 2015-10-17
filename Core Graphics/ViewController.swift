ebab2f327628fe70f909ef4d4adbeec41f7f1078
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-10-17 11:05:07 +0700

    Setup Core Graphics drawing context within drawRectangle method.

--- Core Graphics/ViewController.swift
+++ Core Graphics/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK: - Stored Properties
+    
+    var currentDrawType = 0 {
+        willSet {
+            if self.currentDrawType > 5 { self.currentDrawType = 0 }
+        }
+    }
+    
     // MARK: - IBOutlet Properties
     
     @IBOutlet weak var imageView: UIImageView!
@@ -17,13 +25,22 @@ class ViewController: UIViewController {
     // MARK: - IBAction Properties
     
     @IBAction func redrawActionButton(sender: UIButton) {
+        ++self.currentDrawType
+        
+        switch self.currentDrawType {
+        case 0:
+            self.drawRectangle()
+        default:
+            break
+        }
     }
     
     // MARK: - Methods Override
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.drawRectangle()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,6 +48,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: - Local Methods
+    
+    func drawRectangle() {
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(512, 512), false, 0)
+        guard let currentGraphicsContext = UIGraphicsGetCurrentContext() else { return }
+        
+        let cgRect = CGRect(x: 0, y: 0, width: 512, height: 512)
+        
+        CGContextSetFillColorWithColor(currentGraphicsContext, UIColor.redColor().CGColor)
+        CGContextSetStrokeColorWithColor(currentGraphicsContext, UIColor.blackColor().CGColor)
+        CGContextSetLineWidth(currentGraphicsContext, 10)
+        
+        CGContextAddRect(currentGraphicsContext, cgRect)
+        CGContextDrawPath(currentGraphicsContext, CGPathDrawingMode.FillStroke)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        self.imageView.image = image
+    }
 }
 
